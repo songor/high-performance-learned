e409c30c1ecb0d27d3b4b89f2936d57111aee350
songor <38952103+songor@users.noreply.github.com>
2019-12-13 14:36:26 +0800

    配置 Datasource 和 Druid

--- seckill/src/test/java/com/seckill/SeckillApplicationTests.java
+++ seckill/src/test/java/com/seckill/SeckillApplicationTests.java
@@ -1,13 +1,19 @@
 package com.seckill;
 
+import com.seckill.dao.UserDOMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SeckillApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    UserDOMapper userDOMapper;
+
+    @Test
+    void contextLoads() {
+        userDOMapper.selectByPrimaryKey(1);
+    }
 
 }
