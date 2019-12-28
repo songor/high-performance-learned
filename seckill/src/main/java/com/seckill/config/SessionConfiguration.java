ee0ffae3c12a77dd55cf432b54accaa76a7a071f
songor <38952103+songor@users.noreply.github.com>
2019-12-28 17:52:40 +0800

    注释 spring session 相关代码

--- seckill/src/main/java/com/seckill/config/SessionConfiguration.java
+++ seckill/src/main/java/com/seckill/config/SessionConfiguration.java
@@ -1,9 +1,9 @@
 package com.seckill.config;
 
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.stereotype.Component;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.stereotype.Component;
 
-@Component
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+//@Component
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class SessionConfiguration {
 }
